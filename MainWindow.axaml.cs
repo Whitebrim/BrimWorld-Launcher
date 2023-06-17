0d0b0e345191d9cb6e77e0c89c8b85359f473f0c
Денисов Ярослав <deni7off@yandex.ru>
2023-06-18 02:13:54 +0300

    New background + Titlebar buttons

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,40 +1,73 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Debug = System.Diagnostics.Debug;
 
 namespace Launcher
 {
     public partial class MainWindow : Window
     {
-        private double _previousWidth;
-        private double _previousHeight;
+        private const float windowSize = 0.8f;
+        //private double _previousWidth;
+        //private double _previousHeight;
         public MainWindow()
         {
             InitializeComponent();
-            _previousWidth = Width;
-            _previousHeight = Height;
+            InitialResize();
+            //_previousWidth = Width;
+            //_previousHeight = Height;
         }
 
-        private void OnResized(object? sender, WindowResizedEventArgs e)
+        private void InitialResize()
         {
-            if (e.Reason == WindowResizeReason.User)
+            var screen = Screens.ScreenFromWindow(this).WorkingArea;
+            int newHeight = (int)(Math.Round(screen.Height * windowSize));
+            Height = newHeight;
+            Width = newHeight * 11.0 / 19.0;
+        }
+
+        //private void OnResized(object? sender, WindowResizedEventArgs e)
+        //{
+        //    if (e.Reason == WindowResizeReason.User)
+        //    {
+        //        var screen = Screens.ScreenFromWindow(this).WorkingArea;
+        //        var titlebarHeight = FrameSize.HasValue ? FrameSize.Value.Height - ClientSize.Height : 0;
+        //        if ((int)_previousHeight != (int)e.ClientSize.Height) // Скейлим до 19:11 используя как базу высоту
+        //        {
+        //            Height = Math.Min(screen.Height - titlebarHeight, e.ClientSize.Height);
+        //            Debug.WriteLine(screen.Height - titlebarHeight + " " + e.ClientSize.Height);
+        //            Width = Height * 11 / 19;
+        //        }
+        //        else // Скейлим до 19:11 используя как базу ширину
+        //        {
+        //            Height = Math.Min(screen.Height - titlebarHeight, e.ClientSize.Width * 19 / 11);
+        //            Width = Height * 11 / 19;
+        //        }
+        //    }
+        //    _previousWidth = Width;
+        //    _previousHeight = Height;
+        //}
+
+        private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            BeginMoveDrag(e);
+        }
+
+        private void MinimizeApplication(object? sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ExitApplication(object? sender, RoutedEventArgs e)
+        {
+            var app = Application.Current;
+            if (app?.ApplicationLifetime is ClassicDesktopStyleApplicationLifetime lifetime)
             {
-                var screen = Screens.ScreenFromWindow(this).WorkingArea;
-                var titlebarHeight = FrameSize.HasValue ? FrameSize.Value.Height - ClientSize.Height : 0;
-                if ((int)_previousHeight != (int)e.ClientSize.Height) // Скейлим до 19:11 используя как базу высоту
-                {
-                    Height = Math.Min(screen.Height - titlebarHeight, e.ClientSize.Height);
-                    Debug.WriteLine(screen.Height - titlebarHeight + " " + e.ClientSize.Height);
-                    Width = Height * 11 / 19;
-                }
-                else // Скейлим до 19:11 используя как базу ширину
-                {
-                    Height = Math.Min(screen.Height - titlebarHeight, e.ClientSize.Width * 19 / 11);
-                    Width = Height * 11 / 19;
-                }
+                lifetime.Shutdown();
             }
-            _previousWidth = Width;
-            _previousHeight = Height;
         }
     }
 }
