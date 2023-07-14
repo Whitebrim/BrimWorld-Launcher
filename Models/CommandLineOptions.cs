bba31669d696616d8c13c00f7baf4c93326e256d
Денисов Ярослав <deni7off@yandex.ru>
2023-07-14 09:27:58 +0300

    feat: potential "immutable" fix

--- Models/CommandLineOptions.cs
+++ Models/CommandLineOptions.cs
@@ -7,12 +7,18 @@ public class CommandLineOptions
     [Option('p', "path", Required = false, HelpText = "Launcher data path to save minecraft to")]
     public string Path { get; set; }
 
-    [Option('m', "multiple-launch", Required = false, Default = false, HelpText = "If true, launcher allows to start multiple instances of minecraft")]
+    [Option('m', "multiple-launch", Required = false, Default = (bool)false, HelpText = "If true, launcher allows to start multiple instances of minecraft")]
     public bool MultipleLaunch { get; set; }
 
     public CommandLineOptions()
     {
         Path = "";
         MultipleLaunch = false;
+    }
+
+    public CommandLineOptions(string path, bool multipleLaunch)
+    {
+        Path = path;
+        MultipleLaunch = multipleLaunch;
     }
 }
